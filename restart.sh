be71b5fd59de56d76d2707c83186a5d1248e6407
scysys <scysys@outlook.com>
2023-02-13 16:25:34 +0100

    Initial

---
+++ restart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pkill -f spcast_transcoder.liq
+sleep 3
+chmod +x /home/spcast/SPCast/transcoder/spcast_transcoder.liq
+cd /home/spcast/SPCast/transcoder/spcast_transcoder.liq
+./spcast_transcoder.liq
