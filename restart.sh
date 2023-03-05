8e901554a0ac7d0dd0fe899a13bd900a05a87808
scysys <scysys@outlook.com>
2023-03-05 18:26:36 +0100

    update

--- restart.sh
+++ restart.sh
@@ -3,5 +3,5 @@
 pkill -f spcast_transcoder.liq
 sleep 3
 chmod +x /home/spcast/SPCast/transcoder/spcast_transcoder.liq
-cd /home/spcast/SPCast/transcoder/spcast_transcoder.liq
+cd /home/spcast/SPCast/transcoder
 ./spcast_transcoder.liq
