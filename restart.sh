32c7705cf6ced81edad430cfb9935880135a4a0d
scysys <scysys@outlook.com>
2023-06-07 13:41:31 +0200

    0.2.9

--- restart.sh
+++ restart.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Remove the restart_transcoder file from the secure directory
 rm -f /usr/share/zabbix/sp/secure/.restart_transcoder
+
+# Kill the process with the name haqweawhvjzaam
 pkill -f haqweawhvjzaam
+
+# Wait for 3 seconds
 sleep 3
+
+# Make the spcast_transcoder.liq executable
 chmod +x /home/spcast/SPCast/transcoder/spcast_transcoder.liq
+
+# Change to the transcoder directory
 cd /home/spcast/SPCast/transcoder
+
+# Execute the spcast_transcoder.liq with the name haqweawhvjzaam
 bash -c './spcast_transcoder.liq --name "haqweawhvjzaam"'
